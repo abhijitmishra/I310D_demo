9fc47d58d529150e02f8744d1a6715f7e1fad7f8
Abhijit Mishra <abhijitmishra.530@gmail.com>
2024-11-13 10:35:55 -0600

    Updated with more examples

--- geometry/area_of_a_circle.py
+++ geometry/area_of_a_circle.py
@@ -1,3 +1,5 @@
+# This program is for computing the area of circle
+
 def compute_area_of_circle(radius):
 	pi = 3.14
 	area = pi * radius * radius
@@ -10,3 +12,7 @@ print(f"The area of circle with radius {radius1} is: {area1}")
 radius2 = 40
 area2 = compute_area_of_circle(radius2)
 print(f"The area of circle with radius {radius2} is: {area2}")
+
+radius1 = 60
+area1 = compute_area_of_circle(radius1)
+print(f"The area of circle with radius {radius1} is: {area1}")
